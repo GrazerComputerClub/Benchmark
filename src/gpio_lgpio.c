d96b72356a07b474b26cdf04c4656fc684841fc8
mstroh76 <m.stroh76@gmail.com>
2024-02-02 15:15:52 +0100

    lgpio

---
+++ src/gpio_lgpio.c
@@ -0,0 +1,60 @@
+// GPIO write speed benchmark
+//
+// by GC2 - mstroh
+// Licence: CC BY 4.0
+// Install: sudo apt install liblgpio1 liblgpio-dev
+// Compile: gcc gpio_lgpio.c -o gpio_lgpio -llgpio -Wall
+// Execute: sudo ./gpio_lgpio
+
+
+#include <lgpio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <string.h>
+#include <time.h>
+#include <sys/time.h>
+
+
+const int GPIO = 24;
+const int GPIOChip = 0
+
+
+int main (void){
+	const int  ToggleValue = 10000000;
+	struct timeval t1, t2;
+	double elapsedTime, fTimePerOperation, fFreq;
+
+	printf("lgpio GPIO speed test program (using GPIO %d)\n", GPIO);
+
+	int chip = lgGpiochipOpen(0);
+	if(chip<=0) {
+		printf("ERROR: Failed to get GPIO chip %d.\n", GPIOChip);
+		return 1;
+	}
+	int lFlags = 0;
+	int initLevel = 0
+	if (lgGpioClaimOutput(chip, lFlags, GPIO, initLevel) != LG_OKAY) {
+		printf("ERROR: Failed to get GPIO %d for output.\n", GPIO);
+		lgGpiochipClose(chip);
+		return 1;
+	}
+
+	printf("toggle % 3d million times ...\n", ToggleValue/1000000);
+	gettimeofday(&t1, NULL);
+	for (int loop=1; loop<ToggleValue; loop++) {
+		lgGpioWrite(chip, GPIO, 1);
+		lgGpioWrite(chip, GPIO, 0);
+	}
+	gettimeofday(&t2, NULL);
+	elapsedTime = (t2.tv_sec-t1.tv_sec)+(t2.tv_usec-t1.tv_usec)/1000000.0;
+	fTimePerOperation = elapsedTime*1000000.0/ToggleValue;
+	fFreq = ToggleValue/elapsedTime/1000000.0;
+	printf("  % 9d toggle took %.3f s, Time per toggle %.3f us, Freq %.3f MHz \n",
+	  ToggleValue, elapsedTime, fTimePerOperation, fFreq);
+
+	lgGpioClaimInput(chip, lFlags, GPIO);
+	lgGpiochipClose(chip);
+
+	return(EXIT_SUCCESS);
+}
