a65bfe03c5c7fce70652a223754c05ba6bdf721a
Rafael Diniz <rafael@riseup.net>
2024-03-13 13:39:52 +0000

    fix identation

--- src/gpio_gpiolib.c
+++ src/gpio_gpiolib.c
@@ -20,12 +20,12 @@ unsigned int line_num = 24;	// GPIO #24
 const int  ToggleValue = 240000000;
 
 int main(int argc, char **argv) {
-	int ret;
+    int ret;
 
-	struct timeval t1, t2;
-	double elapsedTime, fTimePerOperation, fFreq;
+    struct timeval t1, t2;
+    double elapsedTime, fTimePerOperation, fFreq;
 
-	printf("GPIOLIB speed test program (using line %d)\n", line_num);
+    printf("GPIOLIB speed test program (using line %d)\n", line_num);
 
     ret = gpiolib_init();
     if (ret < 0)
@@ -48,23 +48,23 @@ int main(int argc, char **argv) {
     gpio_set_drive(line_num, DRIVE_LOW);
     gpio_set_fsel(line_num, GPIO_FSEL_OUTPUT);
 
-	printf("toggle % 3d million times ...\n", ToggleValue/1000000);
+    printf("toggle % 3d million times ...\n", ToggleValue/1000000);
 
-	gettimeofday(&t1, NULL);
-	for (int loop=1; loop<ToggleValue; loop++)
+    gettimeofday(&t1, NULL);
+    for (int loop=1; loop<ToggleValue; loop++)
     {
         gpio_set_drive(line_num, DRIVE_LOW);
         gpio_set_drive(line_num, DRIVE_HIGH);
-	}
-	gettimeofday(&t2, NULL);
-	elapsedTime = (t2.tv_sec-t1.tv_sec)+(t2.tv_usec-t1.tv_usec)/1000000.0;
-	fTimePerOperation = elapsedTime*1000000.0/ToggleValue;
-	fFreq = ToggleValue/elapsedTime/1000000.0;
-	printf("  % 9d toggle took %.3f s, Time per toggle %.3f us, Freq %.3f MHz \n",
-	  ToggleValue, elapsedTime, fTimePerOperation, fFreq);
+    }
+    gettimeofday(&t2, NULL);
+    elapsedTime = (t2.tv_sec-t1.tv_sec)+(t2.tv_usec-t1.tv_usec)/1000000.0;
+    fTimePerOperation = elapsedTime*1000000.0/ToggleValue;
+    fFreq = ToggleValue/elapsedTime/1000000.0;
+    printf("  % 9d toggle took %.3f s, Time per toggle %.3f us, Freq %.3f MHz \n",
+           ToggleValue, elapsedTime, fTimePerOperation, fFreq);
 
     gpio_set_drive(line_num, DRIVE_LOW);
     gpio_set_fsel(line_num, GPIO_FSEL_INPUT);
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
